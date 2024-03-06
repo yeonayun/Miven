8eb778f2777f67127b91d3125d884ed707ebcf2d
yeonayun <w2306@e-mirim.hs.kr>
2024-03-06 21:54:23 +0900

    meven project img

--- member copy.js
+++ member copy.js
@@ -1,6 +1,5 @@
 // 스크롤 이벤트를 감지하여 이미지를 나타내는 함수
 let isFirstScroll = true; // 첫 번째 스크롤 여부를 나타내는 변수
-let bf_scrollY = 0;
 
 function revealImages() {
     const image1 = document.getElementById('num11');
@@ -14,22 +13,6 @@ function revealImages() {
     const image3Top = image3.getBoundingClientRect().top + scrollY;
     const image4Top = image4.getBoundingClientRect().top + scrollY;
 
-    if(bf_scrollY == scrollY){
-        return;
-    }
-    bf_scrollY = scrollY;
-    //console.log("AAA1",image1,image2,image3,image4);
-    // console.log("AAA2",windowHeight,scrollY);
-    // console.log("AAA3",image1Top,image2Top,image3Top,image4Top);
-    // console.log("AAA3",isFirstScroll);
-    // console.log("BBB1",image1Top - windowHeight + 100);
-    // console.log("BBB2",image2Top - windowHeight + 100);
-    // console.log("BBB3",image3Top - windowHeight + 100);
-    // console.log("BBB4",image4Top - windowHeight + 100);
-
-    // 이미지들의 y값 지정하는 계산식 구해주기
-    // scrollY랑 이미지들의 y값이 같으면 사진이 보이게 만들기
-
     // num11이 화면에 나타나는 경우
     if (scrollY > image1Top - windowHeight + 100 && isFirstScroll) {
         image1.style.opacity = '1'; // 투명도를 1로 변경하여 보이게 함
@@ -38,21 +21,21 @@ function revealImages() {
     }
 
     // num11이 화면에 나타나고, 두 번째 스크롤인 경우
-    else if (scrollY > image1Top - windowHeight + 100 && !isFirstScroll && !image2.classList.contains('revealed')) {
+    if (scrollY > image1Top - windowHeight + 100 && !isFirstScroll && !image2.classList.contains('revealed')) {
         image2.style.opacity = '1'; // 투명도를 1로 변경하여 보이게 함
         image2.style.animation = 'floatUp 0.3s ease-in-out forwards'; // floatUp 애니메이션 적용
         image2.classList.add('revealed'); // 이미지가 나타났음을 표시
     }
 
     // num12가 화면에 나타나고, 세 번째 스크롤인 경우
-    else if (scrollY > image2Top - windowHeight + 100 && image2.style.opacity === '1' && !image3.classList.contains('revealed')) {
+    if (scrollY > image2Top - windowHeight + 100 && image2.style.opacity === '1' && !image3.classList.contains('revealed')) {
         image3.style.opacity = '1'; // 투명도를 1로 변경하여 보이게 함
         image3.style.animation = 'floatUp 0.3s ease-in-out forwards'; // floatUp 애니메이션 적용
         image3.classList.add('revealed'); // 이미지가 나타났음을 표시
     }
 
     // num13이 화면에 나타나고, 네 번째 스크롤인 경우
-    else if (scrollY > image3Top - windowHeight + 100 && image3.style.opacity === '1' && !image4.classList.contains('revealed')) {
+    if (scrollY > image3Top - windowHeight + 100 && image3.style.opacity === '1' && !image4.classList.contains('revealed')) {
         image4.style.opacity = '1'; // 투명도를 1로 변경하여 보이게 함
         image4.style.animation = 'floatUp 0.3s ease-in-out forwards'; // floatUp 애니메이션 적용
         image4.classList.add('revealed'); // 이미지가 나타났음을 표시
